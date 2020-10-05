5cca7d26e9fac69b1f55dfb930ef04fe932ca185
Bahdan Pryimachuk <bahdanchik@tut.by>
2020-10-05 13:10:08 +0200

    feat: implement morze decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const wordArr = expr.split('**********');
+    const byLetters = [];
+    wordArr.forEach((item, index) => {
+      const encodedWord = [];
+      for (let i = 0; i < item.length; i += 10) {
+        encodedWord.push(item.substr(i, 10));
+      }
+        
+      const decodedWord = encodedWord.map(item => {
+        let res = '';
+        for (let i = 0; i < item.length; i += 2) {
+          if (item.substr(i, 2) === '10') {
+            res += '.';
+          } else if (item.substr(i, 2) === '11') {
+            res += '-';
+          }
+        }
+        return res;
+      }).map(item => MORSE_TABLE[item]).join('');
+      byLetters.push(decodedWord);
+    });
+    return byLetters.join(' ');
 }
 
 module.exports = {
